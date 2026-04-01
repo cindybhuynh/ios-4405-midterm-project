02428084ef3e903ec0b88195fa51eb30d76afa44
cindybhuynh <chbmv@umsystem.edu>
2026-04-01 15:32:23 -0500

    Initial Commit

---
+++ ios-4405-midterm-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-4405-midterm-project
+//
+//  Created by Cindy Huynh on 4/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-4405-midterm-project/ios_4405_midterm_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_4405_midterm_projectApp.swift
+//  ios-4405-midterm-project
+//
+//  Created by Cindy Huynh on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_4405_midterm_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
